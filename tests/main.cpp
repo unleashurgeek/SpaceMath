a87c9db2080a627ee4bad2e2859b54421a0470af
Kyle Bartz <kyle@unleashurgeek.com>
2015-09-16 23:04:25 -0700

    Updated test

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,14 +7,17 @@ int main()
     vec2<float> t_float(1.6f, 3.5f);
     vec2<int> t_int(5, 4);
 
-    t_float += t_int;
-    t_float += 2;
-    t_float += t_int.yx;
-    t_float.xy += t_int.xy;
-    t_float.xy += t_int.x;
+    //st_float.xy += st_float;
+
+    //t_float += t_int;
+    //t_float += 2;
+    //t_float += t_int.yx;
+    //t_float.xy += t_int.xy;
+    //t_float.xy += t_int.x;
+    t_float = t_float.xy + t_float.yx;
     //t_float.xy += t_int;
 
-    std::cout << t_float.x << ", " << t_float.y << std::endl;
+    std::cout << t_float.r << ", " << t_float.g << std::endl;
     //std::cout << t_float << std::endl;
     std::cin.get();
     return 0;
