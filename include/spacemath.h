a646c80924dfa994288a0060d0ab8cc7e5888712
Kyle Bartz <kyle@unleashurgeek.com>
2015-09-19 18:33:21 -0700

    Merged v2Proxy, v3Proxy, and v4Proxy into vProxy

--- include/spacemath.h
+++ include/spacemath.h
@@ -25,26 +25,39 @@ public:
     }
 };
 
-template<class vT, typename T, int A, int B>
-class v2Proxy : public ArraySubscriptOperators<T>
+template<class vT, typename T, int A, int B, int C, int D>
+class vProxy : public ArraySubscriptOperators<T>
 {
 public:
-    static const bool isWritable = (A != B);
+    static const bool isWritable = (A != B) &&
+                                   ((std::is_same<vT, vec3<T>>::value) ?
+                                   (B != C) && (A != C) : true) &&
+                                   ((std::is_same<vT, vec4<T>>::value) ?
+                                   (C != D) && (B != D) && (A != D) : true);
     typedef typename std::conditional<isWritable,
-        v2Proxy, struct OperationNotValid>::type assignmentType;
+        vProxy, struct OperationNotValid>::type assignmentType;
 
 #   define CREATE_ASSIGNMENT_OPERATOR(Op)                                           \
-    template<class rhsVT, typename rhsT, int rhsA, int rhsB>                        \
-    assignmentType& operator##Op##(const v2Proxy<rhsVT, rhsT, rhsA, rhsB>& _rhs)    \
+    template<class rhsVT, typename rhsT, int rhsA, int rhsB, int rhsC, int rhsD>    \
+    assignmentType& operator##Op##(                                                 \
+                    const vProxy<rhsVT, rhsT, rhsA, rhsB, rhsC, rhsD>& _rhs)        \
     {                                                                               \
         ((T*)this)[A] Op ((const rhsT*)&_rhs)[rhsA];                                \
         ((T*)this)[B] Op ((const rhsT*)&_rhs)[rhsB];                                \
+        if ((sizeof(rhsVT) / sizeof(rhsT) > 2) && (sizeof(vT) / sizeof(T) > 2))     \
+            ((T*)this)[C] Op ((const rhsT*)&_rhs)[rhsC];                            \
+        if ((sizeof(rhsVT) / sizeof(rhsT) > 3) && (sizeof(vT) / sizeof(T) > 3))     \
+            ((T*)this)[D] Op((const rhsT*)&_rhs)[rhsD];                             \
         return *this;                                                               \
     }                                                                               \
     assignmentType& operator##Op##(const T _rhs)                                    \
     {                                                                               \
         ((T*)this)[A] Op _rhs;                                                      \
         ((T*)this)[B] Op _rhs;                                                      \
+        if ((sizeof(vT) / sizeof(T)) > 2)                                           \
+            ((T*)this)[C] Op _rhs;                                                  \
+        if ((sizeof(vT) / sizeof(T)) > 3)                                           \
+            ((T*)this)[D] Op _rhs;                                                  \
         return *this;                                                               \
     }                                                                               \
     template<template<typename> class rhsVT, typename rhsT>                         \
@@ -52,6 +65,10 @@ public:
     {                                                                               \
         ((T*)this)[A] Op _rhs[0];                                                   \
         ((T*)this)[B] Op _rhs[1];                                                   \
+        if ((sizeof(vT) / sizeof(T) > 2) && (sizeof(rhsVT) / sizeof(rhsT) > 2))     \
+            ((T*)this)[C] Op _rhs[2];                                               \
+        if ((sizeof(vT) / sizeof(T) > 3) && (sizeof(rhsVT) / sizeof(rhsT) > 3))     \
+            ((T*)this)[D] Op _rhs[3];                                               \
         return *this;                                                               \
     }
 
@@ -72,12 +89,17 @@ public:
 #   undef CREATE_ASSIGNMENT_OPERATOR
 
 #   define CREATE_ARITHMETIC_OPERATOR(Op)                                           \
-    template<class rhsVT, typename rhsT, int rhsA, int rhsB>                        \
-    vT operator##Op##(const v2Proxy<rhsVT, rhsT, rhsA, rhsB>& _rhs) const           \
+    template<class rhsVT, typename rhsT, int rhsA, int rhsB, int rhsC, int rhsD>    \
+    vT operator##Op##(                                                              \
+       const vProxy<rhsVT, rhsT, rhsA, rhsB, rhsC, rhsD>& _rhs) const               \
     {                                                                               \
         vT result;                                                                  \
         result[0] = ((const T*)this)[A] Op ((const rhsT*)&_rhs)[rhsA];              \
         result[1] = ((const T*)this)[B] Op ((const rhsT*)&_rhs)[rhsB];              \
+        if ((sizeof(vT) / sizeof(T) > 2) && (sizeof(rhsVT) / sizeof(rhsT) > 2))     \
+            result[2] = ((const T*)this[C]) Op ((const rhsT*)&_rhs)[rhsC];          \
+        if ((sizeof(vT) / sizeof(T) > 3) && (sizeof(rhsVT) / sizeof(rhsT) > 3))     \
+            result[3] = ((const T*)this[D]) Op ((const rhsT*)&_rhs)[rhsD];          \
         return result;                                                              \
     }                                                                               \
     vT operator##Op##(const T _rhs) const                                           \
@@ -85,13 +107,21 @@ public:
         vT result;                                                                  \
         result[0] = ((const T*)this)[A] Op _rhs;                                    \
         result[1] = ((const T*)this)[B] Op _rhs;                                    \
+        if ((sizeof(vT) / sizeof(T)) > 2)                                           \
+            result[2] = ((const T*)this)[C] Op _rhs;                                \
+        if ((sizeof(vT) / sizeof(T)) > 3)                                           \
+            result[3] = ((const T*)this)[D] Op _rhs;                                \
         return result;                                                              \
     }                                                                               \
-    friend vT operator##Op##(const T _lhs, const v2Proxy& _rhs)                     \
+    friend vT operator##Op##(const T _lhs, const vProxy& _rhs)                      \
     {                                                                               \
         vT result;                                                                  \
-        result[0] = _lhs Op((const T*)& _rhs)[A];                                   \
-        result[1] = _lhs Op((const T*)& _rhs)[B];                                   \
+        result[0] = _lhs Op ((const T*)&_rhs)[A];                                   \
+        result[1] = _lhs Op ((const T*)&_rhs)[B];                                   \
+        if ((sizeof(vT) / sizeof(T)) > 2)                                           \
+            result[2] = _lhs Op ((const T*)&_rhs)[C];                               \
+        if ((sizeof(vT) / sizeof(T)) > 3)                                           \
+            result[3] = _lhs Op ((const T*)&_rhs)[D];                               \
         return result;                                                              \
     }                                                                               
 
@@ -110,21 +140,34 @@ public:
 #   undef CREATE_ARITHMETIC_OPERATOR
 
 #   define CREATE_COMPARISON_OPERATOR(Op)                                           \
-    template<class rhsVT, typename rhsT, int rhsA, int rhsB>                        \
-    bool operator##Op##(const v2Proxy<rhsVT, rhsT, rhsA, rhsB>& _rhs) const         \
+    template<class rhsVT, typename rhsT, int rhsA, int rhsB, int rhsC, int rhsD>    \
+    bool operator##Op##(                                                            \
+         const vProxy<rhsVT, rhsT, rhsA, rhsB, rhsC, rhsD>& _rhs) const             \
     {                                                                               \
         return (((const T*)this)[A] Op ((const rhsT*)&_rhs)[rhsA]) &&               \
-                    (((const T*)this)[B] Op ((const rhsT*)&_rhs)[rhsB]);            \
+               (((const T*)this)[B] Op ((const rhsT*)&_rhs)[rhsB]) &&               \
+               ((sizeof(vT) / sizeof(T) > 2) && (sizeof(rhsVT) / sizeof(rhsT) > 2) ?\
+               (((const T*)this)[C] Op ((const rhsT*)&_rhs)[rhsC]) : true) &&       \
+               ((sizeof(vT) / sizeof(T) > 3) && (sizeof(rhsVT) / sizeof(rhsT) > 3) ?\
+               (((const T*)this)[C] Op ((const rhsT*)&_rhs)[rhsC]) : true);         \
     }                                                                               \
     bool operator##Op##(const T _rhs) const                                         \
     {                                                                               \
         return (((const T*)this)[A] Op _rhs) &&                                     \
-                    (((const T*)this)[B] Op _rhs);                                  \
+               (((const T*)this)[B] Op _rhs) &&                                     \
+               (((sizeof(vT) / sizeof(T)) > 2) ?                                    \
+               (((const T*)this)[C] Op _rhs) : true) &&                             \
+               (((sizeof(vT) / sizeof(T)) > 3) ?                                    \
+               (((const T*)this)[D] Op _rhs) : true);                               \
     }                                                                               \
-    friend bool operator##Op##(const T _lhs, const v2Proxy& _rhs)                   \
+    friend bool operator##Op##(const T _lhs, const vProxy& _rhs)                    \
     {                                                                               \
         return (_lhs Op ((const T*)&_rhs)[A]) &&                                    \
-                    (_lhs Op ((const T*)&_rhs)[B]);                                 \
+               (_lhs Op ((const T*)&_rhs)[B]) &&                                    \
+               (((sizeof(vT) / sizeof(T)) > 2) ?                                    \
+               (_lhs Op ((const T*)&_rhs)[C]) : true) &&                            \
+               (((sizeof(vT) / sizeof(T)) > 3) ?                                    \
+               (_lhs Op ((const T*)&_rhs)[C]) : true);                              \
     }
 
     CREATE_COMPARISON_OPERATOR(== );
@@ -137,25 +180,6 @@ public:
 #   undef CREATE_COMPARISON_OPERATOR
 };
 
-template<class vT, typename T, int A, int B, int C>
-class v3Proxy : public ArraySubscriptOperators<T>
-{
-public:
-    static const bool isWritable = (A != B) && (B != C) && (A != C);
-    typedef typename std::conditional<isWritable,
-        v3Proxy, struct OperationNotValid>::type assignmentType;
-};
-
-template<class vT, typename T, int A, int B, int C, int D>
-class v4Proxy : public ArraySubscriptOperators<T>
-{
-public:
-    static const bool isWritable = (A != B) && (B != C) && (C != D)
-                                   && (A != C) && (A != D) && (B != D);
-    typedef typename std::conditional<isWritable,
-        v4Proxy, struct OperationNotValid>::type assignmentType;
-};
-
 template<typename T>
 class vec2 : public ArraySubscriptOperators<T>
 {
@@ -166,10 +190,10 @@ public:
         struct { T r, g; };
         struct { T s, t; };
 
-        v2Proxy<vec2, T, 0, 0> xx, rr, ss;
-        v2Proxy<vec2, T, 0, 1> xy, rg, st;
-        v2Proxy<vec2, T, 1, 0> yx, gr, ts;
-        v2Proxy<vec2, T, 1, 1> yy, gg, tt;
+        vProxy<vec2, T, 0, 0, -1, -1> xx, rr, ss;
+        vProxy<vec2, T, 0, 1, -1, -1> xy, rg, st;
+        vProxy<vec2, T, 1, 0, -1, -1> yx, gr, ts;
+        vProxy<vec2, T, 1, 1, -1, -1> yy, gg, tt;
     };
 
     vec2() { }
@@ -180,8 +204,8 @@ public:
         ((T*)this)[0] = ((const iT*)&_v)[0];
         ((T*)this)[1] = ((const iT*)&_v)[1];
     }
-    template<typename pT, int A, int B>
-    vec2(v2Proxy<vec2, pT, A, B>& _v)
+    template<typename pT, int A, int B, int C, int D>
+    vec2(vProxy<vec2, pT, A, B, C, D>& _v)
     {
         ((T*)this)[0] = ((const pT*)&_v)[A];
         ((T*)this)[1] = ((const pT*)&_v)[B];
@@ -201,8 +225,8 @@ public:
         ((T*)this)[1] Op _rhs;                                                      \
         return *this;                                                               \
     }                                                                               \
-    template<class rhsVT, typename rhsT, int rhsA, int rhsB>                        \
-    vec2& operator##Op##(const v2Proxy<rhsVT, rhsT, rhsA, rhsB>& _rhs)              \
+    template<class rhsVT, typename rhsT, int rhsA, int rhsB, int rhsC, int rhsD>    \
+    vec2& operator##Op##(const vProxy<rhsVT, rhsT, rhsA, rhsB, rhsC, rhsD>& _rhs)   \
     {                                                                               \
         ((T*)this)[0] Op ((const rhsT*)&_rhs)[rhsA];                                \
         ((T*)this)[1] Op ((const rhsT*)&_rhs)[rhsB];                                \
@@ -226,17 +250,19 @@ public:
 #   undef CREATE_ASSIGNMENT_OPERATOR
 
 #   define CREATE_ARITHMETIC_OPERATOR(Op)                                           \
-    template<class rhsVT, typename rhsT, int rhsA, int rhsB>                        \
-    vec2 operator##Op##(const v2Proxy<rhsVT, rhsT, rhsA, rhsB>& _rhs) const         \
+    template<class rhsVT, typename rhsT, int rhsA, int rhsB, int rhsC, int rhsD>    \
+    vec2 operator##Op##(                                                            \
+        const vProxy<rhsVT, rhsT, rhsA, rhsB, rhsC, rhsD>& _rhs) const              \
     {                                                                               \
         vec2 result;                                                                \
         result[0] = ((const T*)this)[0] Op ((const rhsT*)&_rhs)[rhsA];              \
         result[1] = ((const T*)this)[1] Op ((const rhsT*)&_rhs)[rhsB];              \
         return result;                                                              \
     }                                                                               \
-    template<class lhsVT, typename lhsT, int lhsA, int lhsB>                        \
-    friend vec2 operator##Op##(const v2Proxy<lhsVT, lhsT, lhsA, lhsB>& _lhs,        \
-                               const vec2& _rhs)                                    \
+    template<class lhsVT, typename lhsT, int lhsA, int lhsB, int lhsC, int lhsD>    \
+    friend vec2 operator##Op##(                                                     \
+                const vProxy<lhsVT, lhsT, lhsA, lhsB, lhsC, lhsD>& _lhs,            \
+                const vec2& _rhs)                                                   \
     {                                                                               \
         vec2 result;                                                                \
         result[0] = ((const lhsT*)&_lhs)[lhsA] Op((const T*)&_rhs)[0];              \
@@ -282,15 +308,17 @@ public:
 #   undef CREATE_ARITHMETIC_OPERATOR
 
 #   define CREATE_COMPARISON_OPERATOR(Op)                                           \
-    template<class rhsVT, typename rhsT, int rhsA, int rhsB>                        \
-    bool operator##Op##(const v2Proxy<rhsVT, rhsT, rhsA, rhsB>& _rhs) const         \
+    template<class rhsVT, typename rhsT, int rhsA, int rhsB, int rhsC, int rhsD>    \
+    bool operator##Op##(                                                            \
+         const vProxy<rhsVT, rhsT, rhsA, rhsB, rhsC, rhsD>& _rhs) const             \
     {                                                                               \
         return (((const T*)this)[0] Op ((const rhsT*)&_rhs)[rhsA]) &&               \
                     (((const T*)this)[1] Op ((const rhsT*)&_rhs)[rhsB]);            \
     }                                                                               \
-    template<class lhsVT, typename lhsT, int lhsA, int lhsB>                        \
-    friend bool operator##Op##(const v2Proxy<lhsVT, lhsT, lhsA, lhsB>& _lhs,        \
-                               const vec2& _rhs)                                    \
+    template<class lhsVT, typename lhsT, int lhsA, int lhsB, int lhsC, int lhsD>    \
+    friend bool operator##Op##(                                                     \
+                const vProxy<lhsVT, lhsT, lhsA, lhsB, lhsC, lhsD>& _lhs,            \
+                const vec2& _rhs)                                                   \
     {                                                                               \
         return (((const lhsT*)&_lhs)[lhsA] Op ((const T*)&_rhs)[0]) &&              \
                     (((const lhsT*)&_lhs)[lhsB] Op ((const T*)&_rhs)[1]);           \
